5a850bf62c9d1b9b23bfd44920f257c9d6a84714
José Antonio <leao-gomes@hotmail.com>
2022-07-18 15:11:11 -0300

    Ajuste para API funcionando

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -4,22 +4,31 @@ import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // acessar a URL
-        String url = "https://imdb-api.com/en/API/Top250Movies/k_4npyfvog";
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         //ref https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/HttpRequest.html
         var request = HttpRequest.newBuilder(endereco).GET().build();
-
-        //extrair somente os dados que interessam (titulo,poster,classificação)
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
+
+        //extrair somente os dados que interessam (titulo,poster,classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
 
         //exbir e manipular os dados (JSON)
-        
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
