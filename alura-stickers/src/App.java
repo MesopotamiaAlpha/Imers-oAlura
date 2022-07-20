8a559288cf8d8502c2f71913237f458343bf8aa4
José Antonio <leao-gomes@hotmail.com>
2022-07-20 08:13:36 -0300

    Revert "Aula 02"
    This reverts commit 38f87d31510f1bd51a8c0fb742587b496928a147.

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,6 +1,4 @@
-import java.io.InputStream;
 import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -20,8 +18,6 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-
-
         //extrair somente os dados que interessam (titulo,poster,classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
@@ -29,23 +25,10 @@ public class App {
 
         //exbir e manipular os dados (JSON)
         for (Map<String,String> filme : listaDeFilmes) {
-
-
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
-            InputStream inputStream = new URL(urlImagem).openStream();
-
-            //tentando arrumar a string para pegar a imagem correta
-            urlImagem = urlImagem.replace("_V1_UX128_CR0,1,128,176_AL_", ".png");
-
-            String nomeArquivo = titulo + ".png";
-
-            var geradora = new GeradoraDeFigurinhas();
-            geradora.cria(inputStream, nomeArquivo);
-
-
             System.out.println("\u001b[1m");
+            System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
             System.out.println();
         }
     }
